79d1785bf43fa46f006ace3b91d5a1893d96b8f4
zigfrei <zigfrei.zf@gmail.com>
2016-10-05 16:06:58 +0300

    Initial commit

---
+++ solution.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+/**
+ * Created by User on 05.10.2016.
+ */
+public class solution {
+    public static void main(String[] args) throws IOException{
+        String content="";
+        String proof;
+        BufferedReader reader =new BufferedReader(new FileReader("d:/123.txt"));
+        while ((proof=reader.readLine())!=null){
+            content+=proof;
+        }
+        reader.close();
+        String mcontent[] =content.split(" ");
+        int sum =Integer.parseInt(mcontent[0]) +Integer.parseInt(mcontent[1]);
+        OutputStream outStr =new FileOutputStream("d:/1234.txt");
+        outStr.write((sum + System.lineSeparator()).getBytes());
+        outStr.close();
+
+    }
+}
