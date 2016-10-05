75d3f2f8607c1072e1898716110ffc251cffbd61
zigfrei <zigfrei.zf@gmail.com>
2016-10-05 16:28:22 +0300

    3

--- solution.java
+++ solution.java
@@ -4,19 +4,21 @@ import java.io.*;
  * Created by User on 05.10.2016.
  */
 public class solution {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) {
         String content="";
         String proof;
+        try{
         BufferedReader reader =new BufferedReader(new FileReader("input.txt"));
         while ((proof=reader.readLine())!=null){
             content+=proof;
         }
-        reader.close();
+        reader.close();}catch (Exception e){}
         String mcontent[] =content.split(" ");
         int sum =Integer.parseInt(mcontent[0]) +Integer.parseInt(mcontent[1]);
+        try{
         OutputStream outStr =new FileOutputStream("output.txt");
         outStr.write((sum + System.lineSeparator()).getBytes());
-        outStr.close();
+        outStr.close();}catch (Exception e){}
 
     }
 }
