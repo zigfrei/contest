98d6f1fca0904d455a98ccfda28f0e5043ba97cb
zigfrei <zigfrei.zf@gmail.com>
2016-10-05 16:11:26 +0300

    input output change

--- solution.java
+++ solution.java
@@ -7,14 +7,14 @@ public class solution {
     public static void main(String[] args) throws IOException{
         String content="";
         String proof;
-        BufferedReader reader =new BufferedReader(new FileReader("d:/123.txt"));
+        BufferedReader reader =new BufferedReader(new FileReader("input.txt"));
         while ((proof=reader.readLine())!=null){
             content+=proof;
         }
         reader.close();
         String mcontent[] =content.split(" ");
         int sum =Integer.parseInt(mcontent[0]) +Integer.parseInt(mcontent[1]);
-        OutputStream outStr =new FileOutputStream("d:/1234.txt");
+        OutputStream outStr =new FileOutputStream("output.txt");
         outStr.write((sum + System.lineSeparator()).getBytes());
         outStr.close();
 
